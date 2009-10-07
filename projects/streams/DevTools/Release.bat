2676276ec4a792f2e35e1d713933fc493c253636
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2009-10-07 20:19:45 +0000

    streams: * Added new DevTools directory. * Created new Release.bat script in DevTools directory. This script is used to create the zip containing release files.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@108 eed78412-d13c-44e9-9ac7-ca5071eb296d

---
+++ projects/streams/DevTools/Release.bat
@@ -0,0 +1,35 @@
+@rem ---------------------------------------------------------------------------
+@rem Stream Extension Classes.
+@rem
+@rem Script used to create zip file containing release files.
+@rem
+@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2009
+@rem
+@rem $Rev$
+@rem $Date$
+@rem ---------------------------------------------------------------------------
+
+@echo off
+
+setlocal
+
+set OutFile=Release\dd-streams.zip
+set DocsDir=Docs
+
+cd .\..
+
+if exist Release rmdir /S /Q Release
+mkdir Release
+
+zip %OutFile% -j -9 PJIStreams.pas
+zip %OutFile% -j -9 PJStreamWrapper.pas
+
+zip %OutFile% -j -9 %DocsDir%\ChangeLog.txt
+zip %OutFile% -j -9 %DocsDir%\MPL.txt
+zip %OutFile% -j -9 %DocsDir%\ReadMe.htm
+zip %OutFile% -j -9 %DocsDir%\Wiki.URL
+
+zip %OutFile% -r -9 .\Demos\*.*
+zip %OutFile% -d Demos\*.svn\*
+
+endlocal
