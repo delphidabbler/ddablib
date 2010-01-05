e6677af63ec65f06208be36555132eecd4bec42d
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2010-01-05 14:06:42 +0000

    stringpe: * Added new DevTools sub-directory containing scripts to help with development process. * Release.bat creates a zip file containing releases. * Tidy.bat removes unwanted temporary and working files.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@293 eed78412-d13c-44e9-9ac7-ca5071eb296d

---
+++ projects/stringpe/DevTools/Release.bat
@@ -0,0 +1,32 @@
+@rem ---------------------------------------------------------------------------
+@rem Extended String Property Editor
+@rem
+@rem Script used to create zip file containing release files.
+@rem
+@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2010
+@rem
+@rem $Rev$
+@rem $Date$
+@rem ---------------------------------------------------------------------------
+
+@echo off
+
+setlocal
+
+set ReleaseDir=Release
+set OutFile=%ReleaseDir%\dd-stringpe.zip
+set DocsDir=Docs
+
+cd .\..
+
+if exist %ReleaseDir% rmdir /S /Q %ReleaseDir%
+mkdir %ReleaseDir%
+
+zip %OutFile% -j -9 PJStringPE.pas
+zip %OutFile% -j -9 PJStringPE.dfm
+
+zip %OutFile% -j -9 %DocsDir%\ChangeLog.txt
+zip %OutFile% -j -9 %DocsDir%\MPL.txt
+zip %OutFile% -j -9 %DocsDir%\ReadMe.htm
+
+endlocal
