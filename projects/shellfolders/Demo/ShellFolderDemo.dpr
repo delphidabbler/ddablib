d9262e9d116ce7a709dbae98d6e04afc7748d474
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2010-01-25 22:42:17 +0000

    shellfolders: * Removed MPL from file header comments and replaced with a simple disclaimer and copyright statement.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@353 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/shellfolders/Demo/ShellFolderDemo.dpr
+++ projects/shellfolders/Demo/ShellFolderDemo.dpr
@@ -1,4 +1,4 @@
-{
+{ 
  * ShellFolderDemo.dpr
  *
  * Project file for Shell Folders Unit demo program.
@@ -6,30 +6,9 @@
  * $Rev$
  * $Date$
  *
- * ***** BEGIN LICENSE BLOCK *****
- *
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- *
- * The Original Code is ShellFolderDemo.dpr.
- *
- * The Initial Developer of the Original Code is Peter Johnson
- * (http://www.delphidabbler.com/).
- *
- * Portions created by the Initial Developer are Copyright (C) 2003-2010 Peter
- * Johnson. All Rights Reserved.
- *
- * Contributor(s):
- *   NONE
- *
- * ***** END LICENSE BLOCK *****
+ * This file is copyright (C) P D Johnson (www.delphidabbler.com), 2003-2010.
+ * It may be used without restriction. This code distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
 }
 
 
