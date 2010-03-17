1a27d260a31e5e3fa2b7aeddf98cd328d5c1592a
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2010-03-17 13:23:31 +0000

    md5: * New batch file that creates a release zip file.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@473 eed78412-d13c-44e9-9ac7-ca5071eb296d

---
+++ projects/md5/DevTools/Release.bat
@@ -0,0 +1,39 @@
+@rem ---------------------------------------------------------------------------
+@rem MD5 Message Digest Unit.
+@rem
+@rem Script used to create zip file containing release files.
+@rem
+@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2010
+@rem
+@rem $Rev$
+@rem $Date$
+@rem ---------------------------------------------------------------------------
+
+@echo off
+
+setlocal
+
+set ReleaseDir=Release
+set OutFile=%ReleaseDir%\dd-md5.zip
+set DocsDir=Docs
+set TestDir=Test
+
+cd .\..
+
+if exist %ReleaseDir% rmdir /S /Q %ReleaseDir%
+mkdir %ReleaseDir%
+
+zip %OutFile% -j -9 PJMD5.pas
+
+zip %OutFile% -j -9 %DocsDir%\ChangeLog.txt
+zip %OutFile% -j -9 %DocsDir%\GPL-2.0.txt
+zip %OutFile% -j -9 %DocsDir%\LGPL-2.1.txt
+zip %OutFile% -j -9 %DocsDir%\MD5-Notice.txt
+zip %OutFile% -j -9 %DocsDir%\MPL-1.1.txt
+zip %OutFile% -j -9 %DocsDir%\ReadMe.htm
+zip %OutFile% -j -9 %DocsDir%\Wiki.URL
+
+zip %OutFile% -r -9 %TestDir%\*.*
+zip %OutFile% -d %TestDir%\*.svn\*
+
+endlocal
