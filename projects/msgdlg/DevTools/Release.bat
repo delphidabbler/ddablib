c537db0ddb176e1423c5fb65b23ff9a5bf3e364b
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2010-08-17 12:44:35 +0000

    msgdlg: * Added new directory of development tools as below. * BuildHelp.bat recompiles the components help file. * Release.bat creates zip file containing files included in a Release.bat. * Tidy.bat removes unwanted temporary and intermediate files and folders.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@545 eed78412-d13c-44e9-9ac7-ca5071eb296d

---
+++ projects/msgdlg/DevTools/Release.bat
@@ -0,0 +1,39 @@
+@rem ---------------------------------------------------------------------------
+@rem Script used to create zip file containing Message Dialog Components release
+@rem
+@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2010
+@rem
+@rem $Rev$
+@rem $Date$
+@rem ---------------------------------------------------------------------------
+
+@echo off
+
+setlocal
+
+cd .\..
+
+set OutFile=Release\dd-messagedlg.zip
+set SrcDir=
+set DocsDir=Docs
+set DemoDir=Demo
+set HelpDir=Help
+
+if exist Release rmdir /S /Q Release
+mkdir Release
+
+zip -j -9 %OutFile% PJMessageDialog.pas
+zip -j -9 %OutFile% PJMessageDialog.dcr
+
+zip -j -9 %OutFile% %HelpDir%\PJMessageDialog.als
+zip -j -9 %OutFile% %HelpDir%\PJMessageDialog.hlp
+
+zip -j -9 %OutFile% %DocsDir%\ChangeLog.txt
+zip -j -9 %OutFile% %DocsDir%\MPL.txt
+zip -j -9 %OutFile% %DocsDir%\ReadMe.htm
+zip -j -9 %OutFile% %DocsDir%\Wiki.URL
+
+zip %OutFile% -r -9 %DemoDir%\*.*
+zip %OutFile% -d %DemoDir%\*.svn\*
+
+endlocal
