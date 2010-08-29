e22bf2efdffe76c7903975ec93a98b56d034b6b0
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2010-08-29 20:20:11 +0000

    lib: * Added new lib/DevTools directory. * Added new Tidy.bat file in lib/DevTools that can delete temporary files and folders from either a specified project or from the whole project directory.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@609 eed78412-d13c-44e9-9ac7-ca5071eb296d

---
+++ lib/DevTools/Tidy.bat
@@ -0,0 +1,68 @@
+@rem ---------------------------------------------------------------------------
+@rem DelphiDabbler Code Library
+@rem
+@rem Script used to delete temporary files and directories in either a single
+@rem project or all projects.
+@rem
+@rem Takes a single parameter which is either the id of the project to be tidied
+@rem or "all", without the quotes, to tidy the complete project directory.
+@rem
+@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2010
+@rem
+@rem $Rev$
+@rem $Date$
+@rem ---------------------------------------------------------------------------
+
+@echo off
+setlocal
+
+echo Tidying
+echo ~~~~~~~
+echo.
+
+if not "%1" == "" goto checkforall
+set ErrorMsg=No command specified: must be project id or "all".
+goto error
+
+:checkforall
+if not "%1" == "all" goto checkid
+set RootDir=..\..\projects
+set InfoMsg=projects directory
+goto start
+
+:checkid
+set RootDir=..\..\projects\%1
+set InfoMsg="%1" project
+if exist %RootDir% goto start
+set ErrorMsg="%1" is not a valid project id
+goto error
+
+:start
+echo Deleting temporary files from %InfoMsg%
+del /S %RootDir%\*.~* 
+del /S %RootDir%\*.bak
+del /S %RootDir%\*.dcu 
+del /S %RootDir%\*.ddp 
+del /S %RootDir%\*.dsk 
+del /S %RootDir%\*.exe 
+del /S /AH %RootDir%\*.GID 
+del /S %RootDir%\*.identcache
+del /S %RootDir%\*.ini
+del /S %RootDir%\*.local
+del /S %RootDir%\*.tmp
+echo.
+
+echo Deleting temporary sub-directories from %InfoMsg%
+if exist %RootDir%\Release rmdir /S /Q %RootDir%\Release
+for /F "usebackq" %%i in (`dir /S /B /A:D %RootDir%\__history*`) do rmdir /S /Q %%i
+echo.
+
+goto end
+
+:error
+echo ** ERROR: %ErrorMsg%
+
+:end
+echo Done.
+
+endlocal
