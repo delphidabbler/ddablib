effdf15463cd4363825a49d7086c32b56cc59798
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2010-10-11 01:54:53 +0000

    hotlabel: * New batch file that compiles help file.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@624 eed78412-d13c-44e9-9ac7-ca5071eb296d

---
+++ projects/hotlabel/DevTools/BuildHelp.bat
@@ -0,0 +1,41 @@
+@rem ---------------------------------------------------------------------------
+@rem Hot Label Component.
+@rem
+@rem Script used to create help file.
+@rem
+@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2010
+@rem
+@rem Requires environment variable HC set to full file path to MS WinHelp
+@rem compiler (HCRTF.exe).
+@rem
+@rem $Rev$
+@rem $Date$
+@rem ---------------------------------------------------------------------------
+
+
+@echo off
+
+setlocal
+
+set HelpDir=..\Help
+set HelpFile=PJHotLabel.hlp
+set ErrorMsg=
+
+if "%HC%" == "" set ErrorMsg=Environment variable HC not set
+if not "%ErrorMsg%" == "" goto error
+
+%HC% -x %HelpDir%\%HelpFile%
+if errorlevel 1 set ErrorMsg=Compilation failed
+if not "%ErrorMsg%"=="" goto error
+goto success
+
+:error
+echo *** ERROR: %ErrorMsg%
+goto end
+
+:success
+echo Succeeded
+
+:end
+
+endlocal
