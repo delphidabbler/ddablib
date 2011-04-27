b4eb8837051810aa9b489b7c8c8c3d12fb423805
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2011-04-27 09:51:20 +0000

    consoleapp: * Provided Echoer with -u switch that enables program to output little endian Unicode text without BOM.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@777 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/consoleapp/Demos/TestApps/Echoer/Echoer.dpr
+++ projects/consoleapp/Demos/TestApps/Echoer/Echoer.dpr
@@ -8,7 +8,7 @@
  * $Rev$
  * $Date$
  *
- * This file is copyright (C) P D Johnson (www.delphidabbler.com), 2007-2010.
+ * This file is copyright (C) P D Johnson (www.delphidabbler.com), 2007-2011.
  * It may be used without restriction. This code distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
 }
@@ -23,16 +23,45 @@ program Echoer;
   Program exit code is always 0.
 
   Usage:
-    Echoer [prefix-text]
-  Eg: to run the with prefix "hello world > " use
+    Echoer [prefix-text] [-u]
+  Eg 1: to run the with prefix "hello world > " and Ansi text output use
     Echoer "hello world > "
+  Eg 2: to run with the prefix "hello world > " and Unicode text output use
+    Echoer "hello world > " -u
+  Eg 3: to run with the default ">" prefix and Ansi text output use
+    Echoer
+  Eg 4: to run with the default ">" prefix and Unicode text output use
+    Echoer -u
+  Note:
+    When the -u switch is specified, any prefix text preceed it.
 }
+
+{$UNDEF COMPILERSUPPORTED}
+{$IFDEF CONDITIONALEXPRESSIONS}
+  {$IF CompilerVersion >= 15.0}   // Delphi 7
+    {$DEFINE COMPILERSUPPORTED}
+  {$IFEND}
+{$ENDIF}
+
+{$IFNDEF COMPILERSUPPORTED}
+  {$MESSAGE FATAL 'Minimum compiler version is Delphi 7'}
+{$ENDIF}
 
 {$APPTYPE CONSOLE}
 
 uses
   Windows, SysUtils;
 
+{$IF not Declared(UnicodeString)}
+type
+  UnicodeString = WideString;
+{$IFEND}
+
+{ Emulates C std lib stdout value by returning appropriate Windows handle }
+function StdOut: Integer;
+begin
+  Result := Windows.GetStdHandle(STD_OUTPUT_HANDLE);
+end;
 
 { Emulates C std lib stderr value by returning appropriate Windows handle }
 function StdErr: Integer;
@@ -40,39 +69,100 @@ begin
   Result := Windows.GetStdHandle(STD_ERROR_HANDLE);
 end;
 
-{ Writes a single character or string to a file }
-procedure WriteStr(Handle: THandle; const S: string);
+{ Writes an Ansi string to an output "file" }
+procedure WriteStr(Handle: THandle; const S: AnsiString); overload;
 var
   Dummy: DWORD;
 begin
-  Windows.WriteFile(Handle, PChar(S)^, Length(S), Dummy, nil);
+  Windows.WriteFile(Handle, Pointer(S)^, Length(S), Dummy, nil);
 end;
 
-{ Writes a single character or string to a file followed by a newline }
-procedure WriteStrLn(Handle: THandle; const S: string);
+{ Writes a Unicode string to an output "file" }
+procedure WriteStr(Handle: THandle; const S: UnicodeString); overload;
+var
+  Dummy: DWORD;
+begin
+  Windows.WriteFile(
+    Handle, Pointer(S)^, Length(S) * SizeOf(WideChar), Dummy, nil
+  );
+end;
+
+{ Writes an Ansi string followed by a newline to an output "file"}
+procedure WriteStrLn(Handle: THandle; const S: AnsiString); overload;
 begin
   WriteStr(Handle, S + #13#10);
 end;
 
-var
-  Prefix: string;
-  Line: string;
-  Count: Integer;
-  ProgName: string;
+{ Writes a Unicode string followed by a newline to an output "file"}
+procedure WriteStrLn(Handle: THandle; const S: UnicodeString); overload;
 begin
-  ProgName := ExtractFileName(ParamStr(0));
-  WriteStrLn(StdErr, ProgName + ' - Starting');
-  Prefix := ParamStr(1);
+  WriteStr(Handle, S + #13#10);
+end;
+
+procedure WriteLine(Handle: THandle; const S: AnsiString; AsUnicode: Boolean);
+begin
+  if AsUnicode then
+    WriteStrLn(Handle, UnicodeString(S))
+  else
+    WriteStrLn(Handle, S);
+end;
+
+function StrToAnsiStr(const S: string): AnsiString;
+{$IFDEF UNICODE}
+var
+  Bytes: TBytes;
+{$ENDIF}
+begin
+  {$IFDEF UNICODE}
+  Bytes := TEncoding.Default.GetBytes(S);
+  SetLength(Result, Length(Bytes));
+  if Length(Bytes) > 0 then
+    Move(Pointer(Bytes)^, Pointer(Result)^, Length(Bytes));
+  {$ELSE}
+  Result := S;
+  {$ENDIF}
+end;
+
+var
+  Prefix: AnsiString;
+  Line: AnsiString;
+  Count: Integer;
+  ProgName: AnsiString;
+  WantUnicode: Boolean;
+
+begin
+  // Parse command line
+  ProgName := StrToAnsiStr(ExtractFileName(ParamStr(0)));
+  if ParamStr(1) <> '-u' then
+    Prefix := StrToAnsiStr(ParamStr(1))
+  else
+    Prefix := '';
   if Prefix = '' then
     Prefix := '>';
-  WriteStrLn(StdErr, ProgName + ' - Using prefix: "' + Prefix + '"');
+  WantUnicode := ((ParamCount = 2) and (ParamStr(2) = '-u'))
+    or ((ParamCount = 1) and (ParamStr(1) = '-u'));
+
+  // Write intro text to stderr
+  WriteLine(StdErr, ProgName + ' - Starting', WantUnicode);
+  WriteLine(
+    StdErr, ProgName + ' - Using prefix: "' + Prefix + '"', WantUnicode
+  );
+
+  // Read lines of Ansi text from stdin and copy to stdout with prefix
   Count := 0;
   while not EOF do
   begin
     Inc(Count);
     ReadLn(Line);
-    WriteLn(Prefix, Line);
+    WriteLine(StdOut, Prefix + Line, WantUnicode);
   end;
-  WriteStrLn(StdErr, ProgName + ' - ' + IntToStr(Count) + ' lines written');
-  WriteStrLn(StdErr, ProgName + ' - Finished');
+
+  // Write closing text to stderr
+  WriteLine(
+    StdErr,
+    ProgName + ' - ' + StrToAnsiStr(IntToStr(Count)) + ' lines written',
+    WantUnicode
+  );
+  WriteLine(StdErr, ProgName + ' - Finished', WantUnicode);
 end.
+
