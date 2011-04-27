6b7926a3f684cdd323525413853f3ff05eb5d52b
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2011-04-27 15:04:50 +0000

    consoleapp: * Fixed demo 7 to work correctly with Unicode Delphis.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@780 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/consoleapp/Demos/Demo7/FmDemo7.pas
+++ projects/consoleapp/Demos/Demo7/FmDemo7.pas
@@ -7,7 +7,7 @@
  * $Rev$
  * $Date$
  *
- * This file is copyright (C) P D Johnson (www.delphidabbler.com), 2007-2010.
+ * This file is copyright (C) P D Johnson (www.delphidabbler.com), 2007-2011.
  * It may be used without restriction. This code distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
 }
@@ -18,9 +18,9 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs,
+  Dialogs, StdCtrls,
 
-  PJPipe, PJConsoleApp, StdCtrls;
+  PJPipe, PJConsoleApp;
 
 type
   TForm1 = class(TForm)
@@ -50,11 +50,16 @@ var
 begin
   fOutStream := nil;
   fOutPipe := nil;
-  // Write memo 1 contents into read pipe
+  // Write memo 1 contents as Ansi text into read pipe. Must be ANSI text
+  // because Echoer.exe requires Ansi input.
   Text := Memo1.Text;
   InPipe := TPJPipe.Create(Length(Text));
   try
+    {$IFDEF UNICODE}
+    InPipe.WriteBytes(TEncoding.Default.GetBytes(Text));
+    {$ELSE}
     InPipe.WriteData(PChar(Text)^, Length(Text));
+    {$ENDIF}
     InPipe.CloseWriteHandle;
     // Create out pipe and stream that receives out pipe's data
     fOutPipe := TPJPipe.Create;
@@ -73,7 +78,9 @@ begin
     finally
       App.Free;
     end;
-    // Load data from output stream into memo 2
+    // Load data from output stream into memo 2: Echoer.exe writes output as
+    // ANSI text. OK on Unicode Delphis because following LoadFromStream call
+    // defaults to Default (ANSI) encoding if no encoding specified.
     fOutStream.Position := 0;
     Memo2.Lines.LoadFromStream(fOutStream);
   finally
