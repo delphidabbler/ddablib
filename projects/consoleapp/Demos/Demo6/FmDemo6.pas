0c8e612b239a6502afa2fffb17d4a9e531bb564c
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2011-04-28 16:41:46 +0000

    consoleapp: * Revised demo 6 to use TPJFileHandle to open redirect files with inheritable handles.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@790 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/consoleapp/Demos/Demo6/FmDemo6.pas
+++ projects/consoleapp/Demos/Demo6/FmDemo6.pas
@@ -7,7 +7,7 @@
  * $Rev$
  * $Date$
  *
- * This file is copyright (C) P D Johnson (www.delphidabbler.com), 2007-2010.
+ * This file is copyright (C) P D Johnson (www.delphidabbler.com), 2007-2011.
  * It may be used without restriction. This code distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
 }
@@ -35,55 +35,11 @@ var
 implementation
 
 uses
-  PJConsoleApp;
+  PJConsoleApp, PJFileHandle;
 
 {$R *.dfm}
 
 { TForm1 }
-
-function CreateOutputFile(const FileName: string): THandle;
-var
-  Security: TSecurityAttributes;  // file's security attributes
-begin
-  // Set up security structure so file handle is inheritable (NT)
-  Security.nLength := SizeOf(Security);
-  Security.lpSecurityDescriptor := nil;
-  Security.bInheritHandle := True;
-  // Create new empty file
-  Result := CreateFile(
-    PChar(FileName),        // file name
-    GENERIC_WRITE,          // writeable file
-    0,                      // no sharing
-    @Security,              // security attributes (NT only)
-    CREATE_ALWAYS,          // always create the file, even if exists
-    FILE_ATTRIBUTE_NORMAL,  // just the normal attributes
-    0                       // no template file (NT only)
-  );
-  if Result = INVALID_HANDLE_VALUE then
-    raise Exception.CreateFmt('Can''t create file "%s"', [FileName]);
-end;
-
-function OpenInputFile(const FileName: string): THandle;
-var
-  Security: TSecurityAttributes;  // file's security attributes
-begin
-  // Set up security structure so file handle is inheritable (NT)
-  Security.nLength := SizeOf(Security);
-  Security.lpSecurityDescriptor := nil;
-  Security.bInheritHandle := True;
-  // Open the file for reading
-  Result := CreateFile(
-    PChar(FileName),        // file name
-    GENERIC_READ,           // readable file
-    FILE_SHARE_READ,        // share read access
-    @Security,              // security attributes (NT only)
-    OPEN_EXISTING,          // file must exist
-    FILE_ATTRIBUTE_NORMAL,  // just the normal attributes
-    0                       // no template file (NT only)
-  );
-  if Result = INVALID_HANDLE_VALUE then
-    raise Exception.CreateFmt('Can''t open file "%s"', [FileName]);
-end;
 
 procedure TForm1.Button1Click(Sender: TObject);
 const
@@ -91,25 +47,34 @@ const
   cOutFile = 'Demo6-out.txt';
 var
   App: TPJConsoleApp;
+  InFile, OutFile: TPJFileHandle;
 begin
   // Save Memo1 to file
+  // OK for Unicode Delphis since SaveToFile defaults to saving as ANSI text
+  // which is what Echoer program needs in input file.
   Memo1.Lines.SaveToFile(cInFile);
   // Execute the application
+  InFile := nil;
+  OutFile := nil;
   App := TPJConsoleApp.Create;
   try
+    InFile := TPJFileHandle.Create(cInFile, fmOpenRead or fmShareDenyNone);
+    OutFile := TPJFileHandle.Create(cOutFile, fmCreate or fmShareExclusive);
     App.Visible := False;
-    App.StdIn := OpenInputFile(cInFile);
-    App.StdOut := CreateOutputFile(cOutFile);
+    App.StdIn := InFile.Handle;
+    App.StdOut := OutFile.Handle;
     if not App.Execute('Echoer ">>> "') then
       raise Exception.CreateFmt(
         'Error %X: %s', [App.ErrorCode, App.ErrorMessage]
       );
   finally
-    FileClose(App.StdIn);
-    FileClose(App.StdOut);
+    OutFile.Free;
+    InFile.Free;
     App.Free;
   end;
   // Load Memo2 from file
+  // OK for Unicode Delphis since Echoer will have written ANSI text to output
+  // file and LoadFromFile defaults to reading ANSI text.
   Memo2.Lines.LoadFromFile(cOutFile);
 end;
 
