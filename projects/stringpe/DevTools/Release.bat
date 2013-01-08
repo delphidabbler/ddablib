3026cc3c4a26c1959abd704df0806dc96de194e1
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2013-01-08 17:28:36 +0000

    stringpe: * Added public domain license.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1067 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/stringpe/DevTools/Release.bat
+++ projects/stringpe/DevTools/Release.bat
@@ -3,7 +3,8 @@
 @rem
 @rem Script used to create zip file containing release files.
 @rem
-@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2010-2011
+@rem Any copyright in this file is dedicated to the Public Domain.
+@rem http://creativecommons.org/publicdomain/zero/1.0/
 @rem
 @rem $Rev$
 @rem $Date$
