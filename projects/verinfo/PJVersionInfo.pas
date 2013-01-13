6861b20bce1d56da482299ab19e3a262dff3f9ed
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2013-01-13 23:26:17 +0000

    verinfo: * Changed files to Mozilla Public License v2.0
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1110 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/verinfo/PJVersionInfo.pas
+++ projects/verinfo/PJVersionInfo.pas
@@ -1,37 +1,15 @@
 {
- * PJVersionInfo.pas
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
  *
- * Version Information Component (32 bit). The component reads version
- * information from files.
+ * Copyright (C) 1998-2013, Peter Johnson (www.delphidabbler.com).
  *
  * $Rev$
  * $Date$
  *
- *
- * ***** BEGIN LICENSE BLOCK *****
- *
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- * 
- * The Original Code is PJVersionInfo.pas.
- * 
- * The Initial Developer of the Original Code is Peter Johnson
- * (http://www.delphidabbler.com/).
- * 
- * Portions created by the Initial Developer are Copyright (C) 1998-2010 Peter
- * Johnson. All Rights Reserved.
- * 
- * Contributor(s):
- *   Roland Beduerftig (C++ builder compatibility)
- * 
- * ***** END LICENSE BLOCK *****
+ * Version Information Component. The component reads version information from
+ * executable files.
 }
 
 
@@ -46,7 +24,7 @@ unit PJVersionInfo;
 //                              operator overloads etc. supported (Delphi 2006
 //                              and later).
 // * Supports_RTLNameSpaces   - Defined if Delphi RTL / VCL unit references
-//                              should be qualified with namespaces. 
+//                              should be qualified with namespaces.
 {$DEFINE Supports_Assert}
 {$DEFINE Supports_ResourceString}
 {$UNDEF Supports_AdvancedRecords}
