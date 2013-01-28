57141105f9346bd255366e01b865cbb91e7528ac
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2013-01-28 16:51:56 +0000

    streams: * Fixed bug in reading memory streams into Unicode strings. * Some reformatting and commenting tweaks.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1296 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/streams/Demos/IStreamWrapDemo/FmIStreamWrap.pas
+++ projects/streams/Demos/IStreamWrapDemo/FmIStreamWrap.pas
@@ -61,9 +61,7 @@ type
   private
     fIMemStm: IStream;  // wraps a memory stream
     procedure UpdateStatusBar(const PanelID: Integer; const Msg: string);
-      {Updates given panel in status bar with given text}
     procedure ShowMemStmSize;
-      {Displays size of memory stream in status bar}
   end;
 
 var
@@ -74,7 +72,7 @@ implementation
 uses
   // Delphi
   Windows, SysUtils, ComObj, Dialogs,
-  // delphiDabbler Stream Library
+  // DelphiDabbler Stream Library
   PJIStreams,
   // Project
   FmIStreamWrapHelp;
@@ -87,20 +85,17 @@ const
 { Helper routines }
 
 function WinFileTimeToDOSDateTime(const FileTime: TFileTime): Integer;
-  {Convert date-time from Windows FILETIME format to DOS date time format,
-  adjusting for local time zone}
 var
   LocalFileTime: TFileTime;
 begin
   FileTimeToLocalFileTime(FileTime, LocalFileTime);
-  if not FileTimeToDOSDateTime(LocalFileTime,
-    LongRec(Result).Hi, LongRec(Result).Lo) then
+  if not FileTimeToDOSDateTime(
+    LocalFileTime, LongRec(Result).Hi, LongRec(Result).Lo
+  ) then
     Result := 0;
 end;
 
 function WinFileTimeToDateTime(const FileTime: TFileTime): TDateTime;
-  {Convert date-time from Windows FILETIME format to TDateTime format, adjusting
-  for local time zone}
 begin
   try
     Result := FileDateToDateTime(WinFileTimeToDOSDateTime(FileTime));
@@ -110,8 +105,6 @@ begin
 end;
 
 function FileTimeFmt(const Date: TFileTime): string;
-  {Return string representation of date-time in Windows FILETIME format, or
-  return 'N/A' if date-time structure is zero}
 begin
   if (Date.dwLowDateTime = 0) and (Date.dwHighDateTime = 0) then
     Result := 'N/A'
@@ -120,7 +113,6 @@ begin
 end;
 
 procedure ShowStats(Stm: IStream);
-  {Display stats for given stream in a dialog box}
 var
   Malloc: IMalloc;  // COM allocator
   Stats: TStatStg;  // stream stats structure
@@ -157,18 +149,17 @@ end;
 { TIStreamWrapForm }
 
 procedure TIStreamWrapForm.btnCloseClick(Sender: TObject);
-  {Close the application}
 begin
   Close;
 end;
 
 procedure TIStreamWrapForm.btnFSStatsClick(Sender: TObject);
-  {Display the file stats menu options}
 var
   PopupPos: TPoint;
 begin
   PopupPos := ClientToScreen(
-    Point(btnFSStats.Left, btnFSStats.Top + btnFSStats.Height));
+    Point(btnFSStats.Left, btnFSStats.Top + btnFSStats.Height)
+  );
   mnuFSStats.Popup(PopupPos.X, PopupPos.Y);
 end;
 
@@ -183,20 +174,19 @@ begin
 end;
 
 procedure TIStreamWrapForm.btnMSAppendClick(Sender: TObject);
-  {Append the contents of the left hand memo control to the memory stream}
 begin
   // Write out the contents of the memo to the current stream position
   // we assume this is the end of the stream
   OleCheck(
     fIMemStm.Write(
-      Pointer(memoOutStr.Text), Length(memoOutStr.Text) * SizeOf(Char), nil)
+      Pointer(memoOutStr.Text), Length(memoOutStr.Text) * SizeOf(Char), nil
+    )
   );
   // Display size of stream in status bar
   ShowMemStmSize;
 end;
 
 procedure TIStreamWrapForm.btnMSClearClick(Sender: TObject);
-  {Clear the memory stream}
 begin
   // Clear stream by setting size to zero
   OleCheck(fIMemStm.SetSize(0));
@@ -205,45 +195,43 @@ begin
 end;
 
 procedure TIStreamWrapForm.btnMSReadClick(Sender: TObject);
-  {Read memory stream into right hand memo and display number of bytes read}
 var
-  StreamSize: LargeInt;   // size of stream
-  StreamStart: LargeInt;  // start of stream: this required by seek but not used
+  StreamSize: LargeInt;
+  StreamStart: LargeInt;
   Buf: PChar;
   BytesRead: LongInt;
 begin
   // Find end of stream to get size (could also use IStream.Stat method)
   OleCheck(fIMemStm.Seek(0, STREAM_SEEK_END, StreamSize));
   // Create a buffer to store all of memory stream + terminal #0 byte
-  GetMem(Buf, StreamSize + 1);
+  GetMem(Buf, StreamSize + SizeOf(Char));
   try
     // Zero all memory in buffer: ensures we have terminal #0 byte
-    FillChar(Buf^, StreamSize + 1, 0);
+    FillChar(Buf^, StreamSize + SizeOf(Char), 0);
     // Wind to start of stream and read it in
     OleCheck(
-      fIMemStm.Seek(0, STREAM_SEEK_SET, StreamStart) // we never use StartStream
+      fIMemStm.Seek(0, STREAM_SEEK_SET, StreamStart)
     );
     OleCheck(fIMemStm.Read(Buf, StreamSize, @BytesRead));
     // Copy buffer to memo where we're displaying stream
     memoInStr.Lines.SetText(Buf);
     // Display number of bytes read in status bar
     UpdateStatusBar(1,
-      'Bytes read from Memory Stream = ' + IntToStr(BytesRead));
+      'Bytes read from Memory Stream = ' + IntToStr(BytesRead)
+    );
   finally
     // Free the buffer
-    FreeMem(Buf, StreamSize + 1);
+    FreeMem(Buf, StreamSize);
   end;
 end;
 
 procedure TIStreamWrapForm.btnMSStatsClick(Sender: TObject);
-  {Display the memory stream's stats}
 begin
   // Display stats for memory stream
   ShowStats(fIMemStm);
 end;
 
 procedure TIStreamWrapForm.btnMSToFileClick(Sender: TObject);
-  {Copy whole of memory stream into a file}
 var
   FileStm: TPJIStreamWrapper;     // stream to output file
   MSSize: Int64;                  // size of mem stream
@@ -276,7 +264,6 @@ begin
 end;
 
 procedure TIStreamWrapForm.FormCreate(Sender: TObject);
-  {Create an IStream wrapper to memory stream and show its size (=0)}
 begin
   // Create an IStream wrapper to a memory stream: we use this for tranferring
   // data between left hand memo and right hand memo and/or file
@@ -288,8 +275,6 @@ begin
 end;
 
 procedure TIStreamWrapForm.miFSStatsClick(Sender: TObject);
-  {Open a stream to trest file and display stream's stats info according to
-  user's menu choice}
 var
   MI: TMenuItem;  // menu item selected
   Stm: IStream;   // stream opened to test file
@@ -298,7 +283,8 @@ begin
   // Check file exists and get out if it doesn't
   if not FileExists(cTestFileName) then
   begin
-    MessageDlg('File doesn''t exist.'#10 +
+    MessageDlg(
+      'File doesn''t exist.'#10 +
       'Use the "Copy Memory Stream to File" button to create the file.',
       mtError,
       [mbOK],
@@ -351,7 +337,6 @@ begin
 end;
 
 procedure TIStreamWrapForm.ShowMemStmSize;
-  {Display size of memory stream}
 var
   Stats: TStatStg;  // stats for memory stream
 begin
@@ -371,3 +356,4 @@ begin
 end;
 
 end.
+
