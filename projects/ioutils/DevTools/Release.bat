bb8480c16c2a228e7e888d84a7aeb12c218e3ee0
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2013-03-21 11:57:54 +0000

    ioutils: * Changed name of online documentation internet shortcut file. * Removed commented out code.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1305 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/ioutils/DevTools/Release.bat
+++ projects/ioutils/DevTools/Release.bat
@@ -16,7 +16,6 @@ cd .\..
 
 set OutFile=Release\dd-ioutils.zip
 set DocsDir=Docs
-rem set DemosDir=Demos
 
 if not exist Release mkdir Release
 if exist %OutFile% del %OutFile%
@@ -30,8 +29,6 @@ zip %OutFile% -j -9 %DocsDir%\MPL-1.1.txt
 zip %OutFile% -j -9 %DocsDir%\LGPL-2.1.txt
 zip %OutFile% -j -9 %DocsDir%\GPL-2.0.txt
 zip %OutFile% -j -9 %DocsDir%\ReadMe.htm
-zip %OutFile% -j -9 %DocsDir%\DocumentationWiki.URL
-
-rem zip %OutFile% -r -9 %DemosDir%\*.* -x *.svn\*
+zip %OutFile% -j -9 %DocsDir%\Documentation.URL
 
 endlocal
