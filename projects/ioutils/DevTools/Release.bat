2eae6425997209ea99f84f391428404346e6d5de
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2013-03-21 12:48:11 +0000

    ioutils: * Removed copyright and added public domain CC0 dedication.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1310 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/ioutils/DevTools/Release.bat
+++ projects/ioutils/DevTools/Release.bat
@@ -1,8 +1,10 @@
 @rem ---------------------------------------------------------------------------
-@rem Script used to create zip file containing release files for I/O Utilities
-@rem Classes.
+@rem I/O Utility Classes.
 @rem
-@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2012
+@rem Script used to create zip file containing release files.
+@rem
+@rem Any copyright in this file is dedicated to the Public Domain.
+@rem http://creativecommons.org/publicdomain/zero/1.0/
 @rem
 @rem $Rev$
 @rem $Date$
