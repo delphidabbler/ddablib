d3f92937e9f912fafe12df1680741ed379305857
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2013-03-23 16:17:25 +0000

    consoleapp: * Removed PJPipes.pas, PJPipeFilters.pas and PJFileHandle.pas from files includes in release zip.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1336 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/consoleapp/DevTools/Release.bat
+++ projects/consoleapp/DevTools/Release.bat
@@ -24,9 +24,6 @@ if not exist Release mkdir Release
 if exist %OutFile% del %OutFile%
 
 zip %OutFile% -9 PJConsoleApp.pas
-zip %OutFile% -9 PJPipe.pas
-zip %OutFile% -9 PJPipeFilters.pas
-zip %OutFile% -9 PJFileHandle.pas
 
 zip %OutFile% -j -9 %DocsDir%\ChangeLog.txt
 zip %OutFile% -j -9 %DocsDir%\MPL.txt
