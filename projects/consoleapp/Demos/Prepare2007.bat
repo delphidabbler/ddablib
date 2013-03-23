20bfa5be1c5766e6d474c8387d944500d92faff1
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2013-03-23 16:13:24 +0000

    consoleapp: * Dedicated batch files to public domain.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1334 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/consoleapp/Demos/Prepare2007.bat
+++ projects/consoleapp/Demos/Prepare2007.bat
@@ -1,3 +1,16 @@
+@rem ---------------------------------------------------------------------------
+@rem Console Application Runner Classes.
+@rem
+@rem Script used to create correct format .dproj files for compilation of demos
+@rem with Delphi 2007.
+@rem
+@rem Any copyright in this file is dedicated to the Public Domain.
+@rem http://creativecommons.org/publicdomain/zero/1.0/
+@rem
+@rem $Rev$
+@rem $Date$
+@rem ---------------------------------------------------------------------------
+
 @echo off
 echo Updating .dproj files for Delphi 2007
 echo.
--- projects/consoleapp/DevTools/Release.bat
+++ projects/consoleapp/DevTools/Release.bat
@@ -1,8 +1,10 @@
 @rem ---------------------------------------------------------------------------
-@rem Script used to create zip file containing release files for Console
-@rem Application Runner Classes.
+@rem Console Application Runner Classes.
 @rem
-@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2007-2010
+@rem Script used to create zip file containing release files.
+@rem
+@rem Any copyright in this file is dedicated to the Public Domain.
+@rem http://creativecommons.org/publicdomain/zero/1.0/
 @rem
 @rem $Rev$
 @rem $Date$
