d422c85b1b194f19189b018230b1a2609d6785d8
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2014-01-12 04:55:22 +0000

    hkeype: * Dedicated batch file to public domain using Creative Commons CC0 "license".
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1571 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/hkeype/DevTools/Release.bat
+++ projects/hkeype/DevTools/Release.bat
@@ -1,9 +1,9 @@
 @rem ---------------------------------------------------------------------------
-@rem HKEY Property Editor.
+@rem Script used to a create zip file containing the release files for the HKEY
+@rem Property Editor.
 @rem
-@rem Script used to create zip file containing release files.
-@rem
-@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2009
+@rem Any copyright in this file is dedicated to the Public Domain.
+@rem http://creativecommons.org/publicdomain/zero/1.0/
 @rem
 @rem $Rev$
 @rem $Date$
