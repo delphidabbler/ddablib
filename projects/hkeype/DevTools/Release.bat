cf45ad794e10e38b5004a2107ddcf8a967e3b4db
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2014-01-12 13:19:31 +0000

    hkeype: * Reverted changes from r1585: forgot to branch the final v1.0.5 release.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1586 eed78412-d13c-44e9-9ac7-ca5071eb296d

---
+++ projects/hkeype/DevTools/Release.bat
@@ -0,0 +1,31 @@
+@rem ---------------------------------------------------------------------------
+@rem Script used to a create zip file containing the release files for the HKEY
+@rem Property Editor.
+@rem
+@rem Any copyright in this file is dedicated to the Public Domain.
+@rem http://creativecommons.org/publicdomain/zero/1.0/
+@rem
+@rem $Rev$
+@rem $Date$
+@rem ---------------------------------------------------------------------------
+
+@echo off
+
+setlocal
+
+set ReleaseDir=Release
+set OutFile=%ReleaseDir%\dd-hkeype.zip
+set DocsDir=Docs
+
+cd .\..
+
+if exist %ReleaseDir% rmdir /S /Q %ReleaseDir%
+mkdir %ReleaseDir%
+
+zip %OutFile% -j -9 PJHKEYPropEdit.pas
+
+zip %OutFile% -j -9 %DocsDir%\ChangeLog.txt
+zip %OutFile% -j -9 %DocsDir%\MPL-2.txt
+zip %OutFile% -j -9 %DocsDir%\ReadMe.htm
+
+endlocal
---
+++ projects/hkeype/Docs/HKeyPE-SF-ReadMe.txt
@@ -0,0 +1,12 @@
+HKEY Property Editor
+--------------------
+
+**WARNING** This property editor is deprecated. You are strongly advised NOT to
+use it.
+
+The property editor works erratically. On some Delphi installations it works
+fine while on others the object inspector does not recognise the editor. The
+problem has proved to be intractable, so it has been decided not to continue to
+maintain the code.
+
+There will be no further releases.
