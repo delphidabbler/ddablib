44afb9185ede87b5cfa794cb602ae59399522c25
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2014-01-16 14:41:10 +0000

    envvars: * Re-licensed under MPL 2.0.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1683 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/envvars/PJEnvVars.pas
+++ projects/envvars/PJEnvVars.pas
@@ -1,37 +1,21 @@
 {
- * PJEnvVars.pas
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
  *
- * Source code for environment variable routines and component for processing
- * and managing environment variables.
- *
- * Documentation of this unit is at
- * http://www.delphidabbler.com/url/envvars-wiki
+ * Copyright (C) 1998-2014, Peter Johnson (www.delphidabbler.com).
  *
  * $Rev$
  * $Date$
  *
- * ***** BEGIN LICENSE BLOCK *****
+ * DelphiDabbler Environment Variables unit. Contains a component and several
+ * routines and component for processing and managing environment variables.
  *
- * Version: MPL 1.1
+ * Documented at http://www.delphidabbler.com/url/envvars-docs
  *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ * ACKNOWLEDGEMENTS
  *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- *
- * The Original Code is PJEnvVars.pas
- *
- * The Initial Developer of the Original Code is Peter Johnson
- * (http://www.delphidabbler.com/).
- *
- * Portions created by the Initial Developer are Copyright (C) 2001-2014 Peter
- * Johnson. All Rights Reserved.
- *
- * Contributor(s):
- *   e.e (fixed buffer size error in ExpandEnvVars function)
+ * Thanks to "e.e" for bug fix in v1.3.2
  *
  * ***** END LICENSE BLOCK *****
 }
