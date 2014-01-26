20a6c0e894027c5d9b453d4d729b13a8a27310d8
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2014-01-26 11:25:10 +0000

    envvars: * Added public domain dedication.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1722 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/envvars/Demos/2/VCL/FmSlave.pas
+++ projects/envvars/Demos/2/VCL/FmSlave.pas
@@ -1,3 +1,14 @@
+{
+ * Main form for Slave application of the DelphiDabbler Environment Variables
+ * Unit demo program #2, VCL version.
+ *
+ * $Rev$
+ * $Date$
+ *
+ * Any copyright in this file is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+}
+
 unit FmSlave;
 
 interface
--- projects/envvars/Demos/2/VCL/MasterApp.dpr
+++ projects/envvars/Demos/2/VCL/MasterApp.dpr
@@ -1,3 +1,14 @@
+{
+ * Project filefor Master application of the DelphiDabbler Environment Variables
+ * Unit demo program #2, VCL version.
+ *
+ * $Rev$
+ * $Date$
+ *
+ * Any copyright in this file is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+}
+
 program MasterApp;
 
 uses
