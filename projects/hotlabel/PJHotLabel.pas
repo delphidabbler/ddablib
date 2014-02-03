29753be19d6e30cb01086555c038c496a41d426a
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2014-02-03 06:10:12 +0000

    hotlabel: * Updated source code license from MPL 1.1 to MPL 2.0.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1770 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/hotlabel/PJHotLabel.pas
+++ projects/hotlabel/PJHotLabel.pas
@@ -1,34 +1,17 @@
 {
- * PJHotLabel.pas
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
  *
- * Source of the TPJHotLabel component, which is a label that when clicked opens
- * a URL in the default web browser or email client.
+ * Copyright (C) 1999-2014, Peter Johnson (www.delphidabbler.com).
  *
  * $Rev$
  * $Date$
  *
- * ***** BEGIN LICENSE BLOCK *****
+ * Source code of the TPJHotLabel component, which is a label that when clicked
+ * opens a URL in the default web browser or email client.
  *
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- *
- * The Original Code is PJHotLabel.pas.
- *
- * The Initial Developer of the Original Code is Peter Johnson
- * (http://www.delphidabbler.com/).
- *
- * Portions created by the Initial Developer are Copyright (C) 1999-2014 Peter
- * Johnson. All Rights Reserved.
- *
- * Contributor(s):
- *   NONE
+ * Documented at http://www.delphidabbler.com/url/hotlabel-docs
  *
  * ***** END LICENSE BLOCK *****
 }
