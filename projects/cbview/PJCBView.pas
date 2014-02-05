b94c7cad6a118707ce39c0793a100f6a5360f776
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2014-02-05 19:46:37 +0000

    cbview: * Changed license from MPL 1.1 to MPL 2.0.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1803 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/cbview/PJCBView.pas
+++ projects/cbview/PJCBView.pas
@@ -1,37 +1,16 @@
 {
- * PJCBView.pas
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
  *
- * Clipboard Viewer Component source code. Implements a component that monitors
- * the Windows clipboard and triggers an event whenever the content of the
- * clipboard changes.
+ * Copyright (C) 1999-2014, Peter Johnson (www.delphidabbler.com).
  *
  * $Rev$
  * $Date$
  *
- * ***** BEGIN LICENSE BLOCK *****
- *
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- *
- * The Original Code is PJCBView.pas
- *
- * The Initial Developer of the Original Code is Peter Johnson
- * (http://www.delphidabbler.com/).
- *
- * Portions created by the Initial Developer are Copyright (C) 1999-2014 Peter
- * Johnson. All Rights Reserved.
- *
- * Contributor(s):
- *   NONE.
- *
- * ***** END LICENSE BLOCK *****
+ * Clipboard Viewer Component source code. Implements a component that monitors
+ * the Windows clipboard and triggers an event whenever the content of the
+ * clipboard changes.
 }
 
 
