12924b005a0af142afba1def44c421fb7852e7e0
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2014-02-07 16:55:38 +0000

    msgdlg: * Dedicate demo and release batch files to public domain.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1841 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/msgdlg/Demo/PJMessageDialogDemo.dpr
+++ projects/msgdlg/Demo/PJMessageDialogDemo.dpr
@@ -1,17 +1,12 @@
-{ 
- * PJMessageDialogDemo.dpr
- *
- * Project file for demo program that demonstrates use of Message Dialog
- * Components.
+{
+ * Project file for DelphiDabbler Message Dialogue Components demo program.
  *
  * $Rev$
  * $Date$
  *
- * This file is copyright (C) P D Johnson (www.delphidabbler.com), 2003-2010.
- * It may be used without restriction. This code distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * Any copyright in this file is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
 }
-
 
 program PJMessageDialogDemo;
 
--- projects/msgdlg/DevTools/Release.bat
+++ projects/msgdlg/DevTools/Release.bat
@@ -1,7 +1,9 @@
 @rem ---------------------------------------------------------------------------
-@rem Script used to create zip file containing Message Dialog Components release
+@rem Script used to create zip file containing Message Dialogue Unit release
+@rem files.
 @rem
-@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2010
+@rem Any copyright in this file is dedicated to the Public Domain.
+@rem http://creativecommons.org/publicdomain/zero/1.0/
 @rem
 @rem $Rev$
 @rem $Date$
