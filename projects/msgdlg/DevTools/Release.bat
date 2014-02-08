7b966c199302778a2616a243e6ea59f5bd640e33
delphidabbler <delphidabbler@eed78412-d13c-44e9-9ac7-ca5071eb296d>
2014-02-08 01:30:18 +0000

    msgdlg: * Modified re renamed MPL license file.
    git-svn-id: https://svn.code.sf.net/p/ddablib/code/trunk@1845 eed78412-d13c-44e9-9ac7-ca5071eb296d

--- projects/msgdlg/DevTools/Release.bat
+++ projects/msgdlg/DevTools/Release.bat
@@ -28,7 +28,7 @@ zip -j -9 %OutFile% PJMessageDialog.pas
 zip -j -9 %OutFile% PJMessageDialog.dcr
 
 zip -j -9 %OutFile% %DocsDir%\ChangeLog.txt
-zip -j -9 %OutFile% %DocsDir%\MPL.txt
+zip -j -9 %OutFile% %DocsDir%\MPL-2.txt
 zip -j -9 %OutFile% %DocsDir%\ReadMe.htm
 zip -j -9 %OutFile% %DocsDir%\Documentation.url
 
